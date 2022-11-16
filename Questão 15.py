3273ae4be19873dd4c8cdfd3552055198efab938
EvertonCE78 <111984188+EvertonCE78@users.noreply.github.com>
2022-11-16 12:36:30 -0300

    Add files via upload

--- Questão 15.py
+++ Questão 15.py
@@ -2,3 +2,17 @@
 ### Solicite a idade de 5 pessoas e ao final conte quantas pessoas são maiores de idade, a soma de todas as idades, a média das idades, o valor máximo, o valor mínimo, ordene de forma crescente e decrescente.
 """
 
+idades = []
+contador = 1
+testeidade = 0
+
+while contador <= 5:
+    numero = int(input(f"Digite o {contador}ª idade: "))
+    contador +=1
+    idades.append(numero)
+    if numero >= 18:
+        testeidade +=1
+
+print(f'A soma das idades é: {sum(idades)}')
+print(f'Temos {testeidade} pessoas maiores de idade')
+print(f'A ')
